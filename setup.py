6449bac9b6d219ff012edc511dd10bf480b7a96e
Sergey Batalov <echodiv@outlook.com>
2021-04-09 02:38:34 +0300

    add load base data of load map

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup, Extension
+
+
+def main():
+    setup(name="raycaster",
+          version="0.0.1",
+          description="LIbrary for fake 3d games with pygame",
+          author="Sergey Batalov",
+          author_email="echodiv@outlook.com",
+          ext_modules=[Extension("raycaster", ["raycaster.c"])])
+
+
+if __name__ == "__main__":
+    main()
